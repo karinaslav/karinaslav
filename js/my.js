cea177ff8a1ff6d0e03a3df30f74abae39502ac3
Карина <72276674+karinaslav@users.noreply.github.com>
2020-11-29 20:33:08 +0300

    first my

--- js/my.js
+++ js/my.js
@@ -14,3 +14,26 @@ var wow = new WOW(
     }
   );
   wow.init();
+  let h=document.getElementById("h");
+  let m=document.getElementById("m");
+  let s=document.getElementById("s");
+
+  setInterval(tic,1000);
+  function tic() {
+    let day= new Date();
+    let hours=23-day.getHours();
+    if (hours<10) {
+      hours="0"+hours;
+    }
+    let min=59-day.getMinutes();
+    if (min<10) {
+      min="0"+min;
+    }
+    let sec=59- day.getSeconds(); 
+    if (sec<10) {
+      sec="0"+sec;
+    }
+    h.innerHTML= hours+":";
+    m.innerHTML= min+":";
+    s.innerHTML= sec;
+  }
